c84cc7e65096a39d2a484e555b8e43daf2771dbe
shobhitrastogi <rshobhit255@gmail.com>
2024-03-05 17:02:52 +0530

    Adding Images and Database and Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+// import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
   title: "Imaginify",
-  description: "AI -powered image generator",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -22,12 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables : {colorPrimary :"#624cf5"}
+      variables: { colorPrimary: '#624cf5' }
     }}>
-      <html lang="en">
-        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
-      </html>
+      <>
+        <html lang="en">
+          <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+            {children}
+          </body>
+        </html>
+      </>
     </ClerkProvider>
-
   );
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -26,7 +26,7 @@ export async function getUserById(userId: string) {
 
     const user = await User.findOne({ clerkId: userId });
 
-    if (!user) throw new Error("User not found");
+    if (!user) return null;
 
     return JSON.parse(JSON.stringify(user));
   } catch (error) {
